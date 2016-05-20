45bbab0db56be6c4870098fad71a8728470557e2
Joel Allen <joelallen720@gmail.com>
2016-05-19 21:06:35 -0700

    Add iterateJSONObject() tests with JSON file resources.

---
+++ tests/src/JCore/JSON/iterateJSONObject.cpp
@@ -0,0 +1,70 @@
+#include "JCore/JSON/iterateJSONObject.hpp"
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <gtest/gtest.h>
+
+#include "JCore/FileUtils/loadJSONFile.hpp"
+#include "JCore/TestUtils/validResourcePath.hpp"
+#include "JCore/TestUtils/assertEqualElements.hpp"
+#include "JCore/TestUtils/assertNoThrow.hpp"
+
+
+using std::string;
+using std::vector;
+using std::domain_error;
+
+
+namespace JCore {
+
+
+TEST(iterateJSONObjectTest, validIteratedData) {
+    const vector<string> expectedKeys {
+        "key0",
+        "key1",
+        "key2"
+    };
+
+    const vector<int> expectedValues { 0, 1, 2 };
+    vector<string> actualKeys;
+    vector<int> actualValues;
+    JSON jsonObject = loadJSONFile(validResourcePath("json", "keyValuePairs.json"));
+
+    iterateJSONObject(jsonObject, [&](const string & key, const JSON & value) -> void {
+        actualKeys.push_back(key);
+        actualValues.push_back(value.get<int>());
+    });
+
+    assertEqualElements(expectedKeys, actualKeys);
+    assertEqualElements(expectedValues, actualValues);
+}
+
+
+TEST(iterateJSONObjectTest, unalphabeticalKeysIteratedAlphabetically) {
+    const vector<string> expectedKeys { "a", "b", "c", "d", "e", "f" };
+    vector<string> actualKeys;
+    JSON jsonObject = loadJSONFile(validResourcePath("json", "unalphabeticalKeys.json"));
+
+    iterateJSONObject(jsonObject, [&](const string & key, const JSON &) -> void {
+        actualKeys.push_back(key);
+    });
+
+    assertEqualElements(expectedKeys, actualKeys);
+}
+
+
+TEST(iterateJSONObjectTest, iterateDifferentTypes) {
+    JSON jsonObject = loadJSONFile(validResourcePath("json", "types.json"));
+    const auto blank = [&](const string &, const JSON &) -> void {};
+
+    assertNoThrow([&]() -> void {
+        iterateJSONObject(jsonObject["object"], blank);
+    });
+
+    ASSERT_THROW(iterateJSONObject(jsonObject["list"], blank), domain_error);
+    ASSERT_THROW(iterateJSONObject(jsonObject["key"], blank), domain_error);
+}
+
+
+} // namespace JCore
