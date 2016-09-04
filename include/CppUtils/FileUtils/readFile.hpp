c85f93c16e1af7c9160338de317920172615b88b
Joel Allen <joelallen720@gmail.com>
2016-09-03 20:40:15 -0700

    Replace File with readFile() (because it was basically useless).

---
+++ include/CppUtils/FileUtils/readFile.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace CppUtils {
+
+
+std::string readFile(const std::string & path);
+
+
+} // namespace CppUtils
--- src/CppUtils/JSON/loadJSONFile.cpp
+++ src/CppUtils/JSON/loadJSONFile.cpp
@@ -2,7 +2,7 @@
 
 #include <stdexcept>
 
-#include "CppUtils/FileUtils/File.hpp"
+#include "CppUtils/FileUtils/readFile.hpp"
 #include "CppUtils/Errors/Validators/validateNotEmpty.hpp"
 #include "CppUtils/Errors/fileError.hpp"
 
@@ -19,7 +19,7 @@ JSON loadJSONFile(const string & path) {
     JSON json;
 
     try {
-        json = JSON::parse(File(path).getContent());
+        json = JSON::parse(readFile(path));
     }
     catch (const invalid_argument & error) {
         fileError(path, "loadJSONFile", error.what());
