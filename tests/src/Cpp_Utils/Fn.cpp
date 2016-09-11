c12d95d59e33eacc84dde233e7d67120c9f1c5b5
Joel Allen <joelallen720@gmail.com>
2016-09-11 10:44:26 -0700

    Clean Fn tests.

--- tests/src/Cpp_Utils/Fn.cpp
+++ tests/src/Cpp_Utils/Fn.cpp
@@ -6,6 +6,7 @@
 #include <gtest/gtest.h>
 
 #include "Cpp_Utils/Test.hpp"
+#include "Cpp_Utils/String.hpp"
 
 
 using std::vector;
@@ -23,19 +24,51 @@ namespace Cpp_Utils
 // accumulate() Tests
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-static int accumulator(const int accumulation, const int number)
+static int vector_accumulator(const int accumulation, const int element)
 {
-    return accumulation + number;
+    return accumulation + element;
 }
 
 
-TEST(accumulate_Test, test)
+TEST(accumulate_Test, accumulate_vector)
 {
     const vector<int> numbers { 1, 2, 3 };
-    const vector<int> empty;
+    ASSERT_EQ(6, accumulate(0, numbers, vector_accumulator));
+}
 
-    ASSERT_EQ(6, accumulate(0, numbers, accumulator));
-    ASSERT_EQ(0, accumulate(0, empty, accumulator));
+
+TEST(accumulate_Test, accumulate_empty_vector)
+{
+    ASSERT_EQ(0, accumulate(0, vector<int>(), vector_accumulator));
+}
+
+
+static string map_accumulator(const string accumulation, const pair<string, const int> & element)
+{
+    return accumulation + element.first + ": " + to_string(element.second) + "\n";
+}
+
+
+TEST(accumulate_Test, accumulate_map)
+{
+    const map<string, const int> mapped_data
+    {
+        { "first"  , 1 },
+        { "second" , 2 },
+        { "third"  , 3 },
+    };
+
+    ASSERT_EQ(
+        "first: 1\n"
+        "second: 2\n"
+        "third: 3\n",
+        accumulate<string>("", mapped_data, map_accumulator));
+}
+
+
+TEST(accumulate_Test, accumulate_empty_map)
+{
+    ASSERT_EQ("", accumulate<string>("", map<string, const int>(), map_accumulator));
 }
 
 
@@ -44,6 +77,12 @@ TEST(accumulate_Test, test)
 // transform() Tests
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+static string vector_transformer(const string & element)
+{
+    return element.back() + element + "!";
+}
+
+
 TEST(transform_Test, transform_vector)
 {
     const vector<string> strings
@@ -59,10 +98,19 @@ TEST(transform_Test, transform_vector)
             "1string1!",
             "2string2!",
         },
-        transform<string>(strings, [](const string & element) -> string
-        {
-            return element.back() + element + "!";
-        }));
+        transform<string>(strings, vector_transformer));
+}
+
+
+TEST(transform_Test, transform_empty_vector)
+{
+    assert_equal_elements({}, transform<string>(vector<string>(), vector_transformer));
+}
+
+
+static int map_transformer(const pair<int, int> & element)
+{
+    return element.first + element.second;
 }
 
 
@@ -75,12 +123,13 @@ TEST(transform_Test, transform_map)
         { 6 , 5 },
     };
 
-    assert_equal_elements(
-        { 1, 5, 11 },
-        transform<int>(mapped_numbers, [](const pair<int, int> & numbers) -> int
-        {
-            return numbers.first + numbers.second;
-        }));
+    assert_equal_elements({ 1, 5, 11 }, transform<int>(mapped_numbers, map_transformer));
+}
+
+
+TEST(transform_Test, transform_empty_map)
+{
+    assert_equal_elements({}, transform<int>(map<int, int>(), map_transformer));
 }
 
 
